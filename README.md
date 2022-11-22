9819558a8f4a26c5c704169dc9175722718b157c
beliciabamal237 <bamalbelicia@gmail.com>
2022-11-22 05:45:07 -0800

    replace

--- README.md
+++ README.md
@@ -37,3 +37,5 @@ You don’t have to handle the flag characters
 You don’t have to handle field width
 You don’t have to handle precision
 You don’t have to handle the length modifiers
+
+AUTHOR: bamal massok elysa belicia & jeff(Armand) dongmo
---
+++ printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * printf - produces output according to a format
+ * @c: the character
+ * @s: array of character
+ *
+ * Return: number of the character printed
+ */
+int _printf(const char *format,...)
+{
+	printf(
