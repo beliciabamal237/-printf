435a3f35dc466ffcb4138eeb013b651487a0a395
beliciabamal237 <bamalbelicia@gmail.com>
2022-11-22 06:51:16 -0800

    false

--- printf.c
+++ printf.c
@@ -2,10 +2,21 @@
 /**
  * printf - produces output according to a format
  * @c: the character
- * @s: array of character
+ * @format: a character string
  *
  * Return: number of the character printed
  */
 int _printf(const char *format,...)
 {
-	printf(
+	va_list args;
+	int i, c, n;
+	char *s;
+	i = 0;
+	n = 0;
+
+	va_start(args, int);
+	c = va_args(args,int);
+	printf("%d"; c);
+	return (1);
+	va_end(args);
+}
